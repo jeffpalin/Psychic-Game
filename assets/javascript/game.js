ecf6719fa1e5e3c70294b3cea87cf593d09ffb11
Jeff Palin <jeffpalin@gmail.com>
2017-08-16 16:47:10 -0700

    periodic backup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,71 @@
-// Creating variables to hold the number of wins, losses, and ties. Guesses start at 10
+// Creating variables to hold the number of wins, losses, guesses, and total guesses. Guesses start at 10
 
-	var guesses = 10;
+	
+	var letters = [];
 	var wins = 0;
 	var losses = 0;
+	var guesses = 10;
 
+
+// This function produces a random letter for the computer
+
+	var computerGuess = String.fromCharCode(97 + Math.round(Math.random() * 25));
+	
+
+// getTuneUp: function() {
+ //          alert("Car is ready to go!");
+ //          this.isWorking = true;
 
 // Determines which key was pressed.
+
 	document.onkeyup = function(event) {
     	var userGuess = event.key;
-// This function produces a random letter for the computer
-    	var computerGuess = String.fromCharCode(97 + Math.round(Math.random() * 25));
-   	
 
+    
 
-console.log(computerGuess);
+// Tracks wins, losses, guesses left, and total guesses
 
-// Tracks guesses, wins, and losses
 
 	if (userGuess === computerGuess) {
 		wins++;
-	} else {
-		guesses--;
+		reset();
+	} else if ((userGuess !== computerGuess) && (guesses === 0)) {
+		losses++;
+		letters.push(userGuess);
+	}	else {
+			guesses--;
 	}
 
-   console.log(guesses);
-   console.log(wins);
-   console.log(losses);
+
+//Misc console logging for debugging
+
+	console.log(computerGuess);
+	console.log(userGuess);
+	console.log("--------------------------");
+	console.log(wins);
+	console.log(losses);
+	console.log(guesses);
+	console.log("--------------------------");
+
+//This pulls the data for use in the HTML page
+
+document.querySelector("#wins").innerHTML=wins;
+document.querySelector("#losses").innerHTML=losses;
+document.querySelector("#guesses").innerHTML=guesses;
+document.querySelector("#letters").innerHTML=letters;
 
 
 
+// This function resets the variables when the user wins
 
+	function reset () {
+		document.wins = 0;
+		document.losses = 0;
+		document.guesses = 10;
+		document.letters = [];
+		computerGuess();
 
-// Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+}
+}
+
 
